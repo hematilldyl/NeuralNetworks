3d4dfa3e1761a76d79f1cd2537d901f6bb6bb1d8
hematilldyl <d.hematillake@gmail.com>
2018-03-10 15:32:55 -0500

    Update NBM_DH.py

--- NBM_DH.py
+++ NBM_DH.py
@@ -4,24 +4,24 @@ from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 import numpy as np
 
-dataset = read_csv('D:/all_var_1000_v6.txt', header=0, index_col=0, sep="\t")
+dataset = read_csv('D:/some_data.txt', header=0, index_col=0, sep="\t")
 
 def fetch_data(dataset,n_train_hours):
     values = dataset.values
     train = values[:n_train_hours, :]
     test = values[n_train_hours:, :]
     train_X, train_y = train[:, :-1], train[:, -1]
-    test_X, test_y = test[:, :-1], test[:, -1] #reshape the data to have input,timestamp, outputshape
+    test_X, test_y = test[:, :-1], test[:, -1] 
     train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
     test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
     return train_X,train_y,test_X,test_y
 
-def model_LSTM(train_X):
+def model_LSTM(train_X,activ,dropo):
     model = Sequential()
-    model.add(LSTM(50, input_shape=(train_X.shape[1],train_X.shape[2] ),return_sequences = True)) # Number sample/alpha(Number of )
-    model.add(LSTM(25,return_sequences = True))
+    model.add(LSTM(50, input_shape=(train_X.shape[1],train_X.shape[2] ),dropout=dropo,return_sequences = True)) 
+    model.add(LSTM(35,return_sequences = True))
     model.add(LSTM(10))
-    model.add(Dense(1)) 
+    model.add(Dense(1,activation=str(activ))) 
     return model
 
 def save(model,method):
@@ -60,7 +60,7 @@ def estimate(test_X,train_X,yhat,xy,test_y,train_y):
 '''    
 #COMMENT ONCE WEIGHTS SAVED
 train_X,train_y,test_X,test_y=fetch_data(dataset,700)
-model=model_LSTM(train_X)
+model=model_LSTM(train_X,tanh,0.4)
 model.compile(loss='mae', optimizer='adam')
 model.fit(train_X, train_y, epochs=50, batch_size=100, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 #save(model,'')  
